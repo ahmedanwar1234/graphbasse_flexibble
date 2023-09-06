8de5c355cc7a61b757b6e6519eea5e688ad14277
ahmedanwar123 <ahmedalprenses@gmail.com>
2023-09-06 18:22:20 +0300

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,56 @@
+import { footerLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+const FooterColumn=({title,links}:{title:string,links:string[]})=>{
+    return <div className='footer-column'>
+        <h4 className='  font-semibold'>{title}</h4>
+        <ul className=' flex flex-col gap-2 font-normal'>
+{links.map(link=>{
+    return(
+        <Link href={'/'} key={link}>{link}</Link>
+    )
+
+})}
+        </ul>
+    </div>
+}
+const Footer = () => {
+  return (
+    <footer className=' flexStart footer'>
+<div className='flex flex-col gap-12 w-full'>
+<div className='flex items-start flex-col'>
+    <Image src='/logo-purple.svg'
+    width={115}
+    height={38} alt='flexibble'/>
+    <p className='text-start text-sm font-normal mt-5 max-w-xs'> Flexibble is the worlds leading community for creatives to share grow and get hired</p>
+</div>
+<div className='flex xl:w-full justify-between flex-wrap  gap-12'>
+<FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+
+<div className=' flex  flex-col gap-4'>
+<FooterColumn title={footerLinks[1].title} links={footerLinks[1].links}/>
+<FooterColumn title={footerLinks[2].title} links={footerLinks[2].links}/>
+
+</div>
+<FooterColumn title={footerLinks[3].title} links={footerLinks[3].links}/>
+
+<div className=' flex  flex-col gap-4'>
+<FooterColumn title={footerLinks[4].title} links={footerLinks[4].links}/>
+<FooterColumn title={footerLinks[5].title} links={footerLinks[5].links}/>
+
+</div>
+<FooterColumn title={footerLinks[6].title} links={footerLinks[6].links}/>
+
+</div>
+
+
+<div className='flexBetween footer_copyright'><p>@ 2023 Flexibble. All rights reseved</p><p className='text-gray'><span className='text-black font-semibold'>10,214</span>project submitted</p></div>
+
+</div>
+
+
+    </footer>
+  )
+}
+
+export default Footer
